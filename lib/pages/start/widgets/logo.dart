30d5255ae175c2684f6bf5605cd14a3289271afe
KaitoKobayashi-robot <kaito2003kobayashi@gmail.com>
2025-10-08 12:40:11 +0900

    💄 ロゴを追加

--- lib/pages/start/widgets/logo.dart
+++ lib/pages/start/widgets/logo.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Logo extends StatelessWidget {
   const Logo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("ほめて", style: TextStyle(fontSize: 80));
+    const String assetName = 'assets/images/logo.svg';
+    final Widget svg = SvgPicture.asset(
+      assetName,
+      semanticsLabel: 'Homete LOGO',
+      width: 120,
+      height: 120,
+    );
+    return svg;
   }
 }
